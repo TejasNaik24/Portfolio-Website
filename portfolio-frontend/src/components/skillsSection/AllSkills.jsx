1af53c30cca46694fcc7e19543ced777bc7315d0
tejasnaik <naik.tejas11@gmail.com>
2025-09-15 22:46:46 -0400

    fixed skills

--- portfolio-frontend/src/components/skillsSection/AllSkills.jsx
+++ portfolio-frontend/src/components/skillsSection/AllSkills.jsx
@@ -106,7 +106,7 @@ const librariesFrameworks = [
   },
 ];
 
-const devloperTools = [
+const developerTools = [
   {
     skill: "Git",
     icon: FaGitAlt,
@@ -124,24 +124,24 @@ const devloperTools = [
 const AllSkills = () => {
   const [showLanguages, setShowLanguages] = useState(true);
   const [showLibrariesFrameworks, setShowLibrariesFrameworks] = useState(false);
-  const [showdevloperTools, setShowdevloperTools] = useState(false);
+  const [showDeveloperTools, setShowDeveloperTools] = useState(false);
 
   const setLanguages = () => {
     setShowLanguages(true);
     setShowLibrariesFrameworks(false);
-    setShowdevloperTools(false);
+    setShowDeveloperTools(false);
   };
 
   const setLibrariesFrameworks = () => {
     setShowLanguages(false);
     setShowLibrariesFrameworks(true);
-    setShowdevloperTools(false);
+    setShowDeveloperTools(false);
   };
 
-  const setdevloperTools = () => {
+  const setDeveloperTools = () => {
     setShowLanguages(false);
     setShowLibrariesFrameworks(false);
-    setShowdevloperTools(true);
+    setShowDeveloperTools(true);
   };
 
   return (
@@ -180,17 +180,17 @@ const AllSkills = () => {
           Libraries/Frameworks
         </button>
         <button
-          onClick={setdevloperTools}
-          disabled={showdevloperTools}
+          onClick={setDeveloperTools}
+          disabled={showDeveloperTools}
           className={`w-32 sm:w-auto px-3 py-1 rounded-full text-sm sm:px-4 sm:py-2 sm:text-base md:px-5 md:py-3 md:text-lg font-bold text-white border flex items-center gap-1 transition-all duration-500
             ${
-              showdevloperTools
+              showDeveloperTools
                 ? "bg-gradient-to-r from-lightGrey to-black bg-gray-800 cursor-default hover:none border-darkGrey"
                 : "border-cyan bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 hover:shadow-cyanShadow"
             }
             `}
         >
-          Devloper Tools
+          Developer Tools
         </button>
       </motion.div>
       <div className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-8 my-20">
@@ -224,8 +224,8 @@ const AllSkills = () => {
       </div>
 
       <div className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-8 my-20">
-        {showdevloperTools &&
-          devloperTools.map((item, index) => (
+        {showDeveloperTools &&
+          developerTools.map((item, index) => (
             <motion.div
               key={index}
               variants={fadeIn("up", index * 0.1)}
